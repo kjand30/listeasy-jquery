2633e7d6a742c759423b7bd9bfe43b77534036e8
kjand30 <kjand11@gmail.com>
2015-04-29 17:16:29 -0400

    change up script file

--- script.js
+++ script.js
@@ -4,13 +4,47 @@ var template = function(text) {
 
 var main = function() {
   $('form').submit(function() {
-      
+  
+  var text = $('#todo').val(), 
+			html = template(text); 
+  
+  // console.log(html);     
     
-    
+   $('.list').append(html); 
+   $('#todo').val(""); 
+
+
     return false;  
-  }
+  
+  }); 
   
   
 };
+
+// $('.glyphicon-star').click(function() {
+// 	$(this).toggleClass("active"); 
+
+// }); 
+
+// $('.glyphicon-star').on("click",(function() {
+// 	$(this).toggleClass("active"); 
+
+// }); 
+
+$(".list").on("click",".glyphicon-star",  function() {
+	$(this).toggleClass("active"); 
+}); 
+
+$(".list").on(".click",".glyphicon-remove", function() {
+  $(this).closet("p").remove(); 
+
+  //.closet 
+  //.find 
+
+
+})
+
+}; 
+
 
 $(document).ready(main);
